3b023bb8d72bf92b41c9bc6ffe33abc626204107
mashaelakh <56248434+mashaelakh@users.noreply.github.com>
2019-12-23 09:05:21 +0300

    pushing first day wrok

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,69 @@
+class ProjectsController < ApplicationController
+    before_action :set_project, only: [:show, :edit, :update, :destroy]
+    before_action :authenticate_user!, only: [:edit, :update, :destroy, :create, :new]
+  
+    def index
+      @projects = Project.all
+    end
+  
+    def show
+  
+    end
+  
+    def new
+      @project = current_user.projects.new
+    end
+  
+    def edit
+      @project = current_user.projects.find(params[:id])
+  
+    end
+  
+    def create
+      @project = current_user.projects.new(project_params)
+  
+      respond_to do |format|
+        if @project.save
+          format.html { redirect_to @project, notice: 'project was successfully created.' }
+          format.json { render :show, status: :created, location: @project }
+        else
+          format.html { render :new }
+          format.json { render json: @project.errors, status: :unprocessable_entity }
+        end
+      end
+    end
+    def update
+  
+      @project = current_user.project.find(params[:id])
+      respond_to do |format|
+        if @project.update(project_params)
+          format.html { redirect_to @project, notice: 'Project was successfully updated.' }
+          format.json { render :show, status: :ok, location: @project }
+        else
+          format.html { render :edit }
+          format.json { render json: @project.errors, status: :unprocessable_entity }
+        end
+      end
+    end
+  
+    def destroy
+      @project = current_user.projects.find(params[:id])
+      @project.destroy
+      respond_to do |format|
+        format.html { redirect_to projects_url, notice: 'Project was successfully destroyed.' }
+        format.json { head :no_content }
+      end
+    end
+  
+    private
+      # Use callbacks to share common setup or constraints between actions.
+      def set_project
+        @project = Project.find(params[:id])
+      end
+  
+      # Never trust parameters from the scary internet, only allow the white list through.
+      def project_params
+        params.require(:project).permit(:name, :description, :contactinfo)
+      end
+  end
+  
