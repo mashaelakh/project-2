6e172e9f980b26ef1bef8cb5d1b741d2f4034481
mashaelakh <56248434+mashaelakh@users.noreply.github.com>
2019-12-26 10:29:29 +0300

    re

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :projects do
     resources :comments
-  end
+  end 
+  # get "/projects/:user_id", to: "projects#index", as: "index"
   devise_for :users
   root to: "home#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
